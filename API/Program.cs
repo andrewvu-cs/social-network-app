6b239e2ce6a9bf44e3fd70e5e8aa4c3ed52c1385
Andrew <andrewvu.cs@gmail.com>
2019-11-19 14:14:16 -0800

    able to seed data using Entity Framework config, Dependecny Injection in Program.cs, can retrieve API info

--- API/Program.cs
+++ API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -14,7 +17,27 @@ namespace API
         // Where .NET looks for our Main method which executes our app
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // we want to get a ref to our DbContext
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    // Every time we start our app, we will check to see if there is an avail db,
+                    // if not were going to create a new one
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred during migration");
+                };
+            }
+
+            host.Run();
         }
 
         // Program initialization abstraction
