db06addf498fc0b0a8b1446205e2e9e3942ca038
Andrew <andrewvu.cs@gmail.com>
2019-11-20 11:41:23 -0800

    refactored App code to functional component using React Hooks

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,42 +1,37 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Icon, List } from "semantic-ui-react";
 import "./styles.css";
 import axios from "axios";
 import { IActivity } from "../models/activity";
 
-interface IState {
-  activities: IActivity[]
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-class App extends Component<{}, IState> {
-  state: IState = {
-    activities: [],
-  };
-
-  componentDidMount() {
-    axios.get<IActivity[]>("http://localhost:5000/api/activities").then(response => {
-      console.log(response);
-      this.setState({
-        activities: response.data
+  // the 2nd param [], ensures that our effect only runs once and not every render
+  // componentDiDMount equivalent
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        console.log(response);
+        setActivities(response.data)
       });
-    });
-  }
+  },[]);
+ 
 
-  render() {
-    return (
-      <div>
-        <Header as="h2">
-          <Icon name="users" />
-          <Header.Content>Droo Network</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Droo Hangouts</Header.Content>
+      </Header>
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;
